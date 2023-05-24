f25f2fba0ba3b3399f0d999f094f85289d418050
Antony Thomas <antonythomas993@gmail.com>
2023-05-24 14:10:43 +0530

    Firebase configuration added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,20 +1,21 @@
-import React from 'react'
-import './Navbar.css'
-import {ReactComponent as Avatar } from '../../assets/avatar.svg';
+import React from 'react';
+import './Navbar.css';
+import { ReactComponent as Avatar } from '../../assets/avatar.svg';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <div className='navbar_main'>
-        <div className='app_name'>
-            <span>LinkUp</span>
-        </div>
-        <div className='options'>
-            <span>Home</span>
-            <span>Login</span>
-            <span>
-                <Avatar className='avatar'/>
-            </span>
-        </div> 
+      <div className='app_name'>
+        <span>LinkUp</span>
+      </div>
+      <div className='options'>
+        <span><Link to={'/'}>Home</Link></span>
+        <span><Link to={'/login'}>Login</Link></span>
+        <span>
+          <Avatar className='avatar' />
+        </span>
+      </div>
     </div>
   )
 }
---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjEeo1coNbA0nwFBheXCECNSORTDrFov0",
+  authDomain: "linkup-b0850.firebaseapp.com",
+  projectId: "linkup-b0850",
+  storageBucket: "linkup-b0850.appspot.com",
+  messagingSenderId: "1042096961853",
+  appId: "1:1042096961853:web:b8477f0e51c86fb704ad54",
+  measurementId: "G-5M4VCMQD6P"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
