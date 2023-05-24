00a36d3f128e403b623d6ac7a80f6204e097f00c
Antony Thomas <antonythomas993@gmail.com>
2023-05-24 15:56:11 +0530

    Login UI added

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,19 +1,47 @@
-import {auth, provider} from '../../config/firebase';
-import {signInWithPopup} from 'firebase/auth';
-import {useNavigate} from 'react-router-dom';
+import { auth, provider } from '../../config/firebase';
+import { signInWithPopup } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import './Login.css';
+import { useState } from 'react';
 
 function Login() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const SignInWithGoogle = async() => {
-        const result = await signInWithPopup(auth, provider)
-        console.log(result);
-        navigate('/');
-    }
+  const SignInWithGoogle = async () => {
+    const result = await signInWithPopup(auth, provider)
+    console.log(result);
+    navigate('/');
+  }
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    console.log(username, password);
+  }
   return (
-    <div>
-        <p>Sign In with Google to Continue</p>
-        <button onClick={SignInWithGoogle}>Sign in With Google</button>
+    <div className='login'>
+      <div className='login_main'>
+        <div className='login_text'>Login</div>
+        <div className='username'>
+          <span>Username</span>
+          <input type='text' onChange={(event) => setUsername(event.target.value)} />
+        </div>
+        <div className='password'>
+          <span>Password</span>
+          <input type='password' onChange={(event) => setPassword(event.target.value)}/>
+        </div>
+        <div className='login_button'>
+          <button onClick={handleLogin}>Login</button>
+          <span>OR</span>
+        </div>
+        
+        <div className='google'>
+          <p>Sign In with Google to Continue</p>
+          <button onClick={SignInWithGoogle}>Sign in With Google</button>
+        </div>
+      </div>
+
     </div>
   )
 }
