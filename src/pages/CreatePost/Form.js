f752e5604857fbdf6859a665b10cd040fba796e7
Antony Thomas <antonythomas993@gmail.com>
2023-05-25 11:02:59 +0530

    Firestore and CRUD operations in createPost

--- src/pages/CreatePost/Form.js
+++ src/pages/CreatePost/Form.js
@@ -1,13 +1,53 @@
-import './Form.css';
+import "./Form.css";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import {addDoc, collection} from 'firebase/firestore'
+import { db, auth } from "../../config/firebase";
+import {useAuthState} from 'react-firebase-hooks/auth';
 
 function Form() {
+
+  const [user] = useAuthState(auth);
+
+  const schema = yup.object().shape({
+    title: yup.string().required("Title is necessary"),
+    description: yup.string().required("Please describe your post!"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const postRef = collection(db, "posts");
+
+  const submitCreatePost = async(data) => {
+    console.log(data);
+    await addDoc(postRef, {
+      title: data.title,
+      description: data.description,
+      userId: user?.uid,
+      username: user?.displayName
+    })
+  }
   return (
-    <form className='createpost_form'>
-        <input type='text' placeholder='Title...' className='form_input'/>
-        <textarea type='text' placeholder='Description' className='form_textarea'/>
-        <input type='submit' className='form_submit'/>
+    <form className="createpost_form" onSubmit={handleSubmit(submitCreatePost)}>
+      <input type="text" placeholder="Title..." className="form_input" {...register("title")}/>
+      <span style={{color: "red"}}>{errors.title?.message}</span>
+      <textarea
+        type="text"
+        placeholder="Description"
+        className="form_textarea"
+        {...register("description")}
+      />
+      <span style={{color: "red"}}>{errors.description?.message}</span>
+      <input type="submit" className="form_submit" />
     </form>
-  )
+  );
 }
 
-export default Form
+export default Form;
